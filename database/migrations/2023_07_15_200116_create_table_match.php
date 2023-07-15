547e849de914351d41a176817572f1a03ac2ee55
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-15 16:15:07 -0400

    match partido

---
+++ database/migrations/2023_07_15_200116_create_table_match.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matchs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('Nombre del partido');
+            $table->unsignedBigInteger('player1_id')->comment('Jugador 1');
+            $table->unsignedBigInteger('player2_id')->comment('Jugador 2');
+            $table->unsignedBigInteger('winner_id')->comment('Ganador del partido');
+            $table->integer('status')->comment('(1) Esperando; (2) En Progreso; (3) Finalizado');
+
+            $table->foreign("player1_id")
+                ->references("id")
+                ->on("players")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+            
+            $table->foreign("player2_id")
+                ->references("id")
+                ->on("players")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+            
+            $table->foreign("winner_id")
+                ->references("id")
+                ->on("players")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");    
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matchs');
+    }
+};
