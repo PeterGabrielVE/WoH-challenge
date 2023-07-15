e1d08e6f2176576784ee25c62c1ac8bd25c04e29
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-15 11:10:08 -0400

    migracion para tabla de item players

---
+++ database/migrations/2023_07_15_150627_create_table_item_for_player.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('itemForPlayer', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('player_id');
+            $table->unsignedBigInteger('item_id');
+            
+            $table->foreign("player_id")
+                ->references("id")
+                ->on("players")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+
+            $table->foreign("item_id")
+                ->references("id")
+                ->on("items")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('itemForPlayer');
+    }
+};
