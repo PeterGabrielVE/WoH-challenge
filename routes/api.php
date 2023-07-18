ac993e7494fac177f75baa08b07ed208b6271500
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-18 12:20:13 -0400

    ajustes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AttackController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +21,6 @@ Route::resource('players','PlayerController');
 Route::resource('items','ItemController');
 Route::resource('itemPlayer','ItemPlayerController');
 Route::resource('battles','BattleController');
-Route::get('attacks', [AttackController::class, 'index']);
 
 
 Route::post('createBattle', 'BattleController@start');
